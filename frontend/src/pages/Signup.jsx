49a81299fce9acfd512184b94069925ed6e931c0
YareeYare <mehulshakya230@gmail.com>
2024-03-18 23:25:25 +0530

    creates Signup Page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,25 @@
+import Heading from '../components/Heading'
+import SubHeading from '../components/SubHeading'
+import InputBox from '../components/InputBox'
+import Button from '../components/Button'
+import BottomWarning from '../components/BottomWarning'
+
+const Signup = () => {
+  return <div className="bg-slate-300 h-screen flex justify-center">
+    <div className="flex flex-col justify-center">
+      <div className="rounded-lg bg-white w-80 h-max text-center p-2 px-4 ">
+        <Heading label='Sign Up' />
+        <SubHeading label='Enter your information to create an account' />
+        <InputBox label='First Name' placeholder='Mehul' />
+        <InputBox label='Last Name' placeholder='Shakya' />
+        <InputBox label='Email' placeholder='mehul@gmail.com' />
+        <InputBox label='Password' placeholder='123456' />
+        <Button label='Sign up'/>
+        <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"}/>
+      </div>
+    </div>
+  </div>
+  
+}
+
+export default Signup
